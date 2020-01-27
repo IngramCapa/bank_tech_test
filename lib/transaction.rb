5a65f24378cd35adf04cbe6af573a94acc0bee02
Raluca Ciucu <raluca.ciucu@gmail.com>
2020-01-27 19:55:38 +0000

    feat: transaction records date

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,3 +1,13 @@
 class Transaction
 
+    attr_reader  :date
+
+    def initialize
+        @date = date
+    end
+
+    def format_date(date)
+        date = Time.new
+        "#{date.day}/#{date.month}/#{date.year}"
+    end
 end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -1,7 +1,15 @@
 require 'transaction'
+require 'money'
 
 describe Transaction do
   
-  
+  let(:subject){Transaction.new}
+
+  describe "#date" do
+
+    it "records a transaction date in European format" do
+      allow(subject).to receive(:format_date).and_return("10/01/2012")
+    end
+  end
 
 end
