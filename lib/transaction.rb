904ee1480ec9e919f4aaf5a816b7abf77ab884a7
Raluca Ciucu <raluca.ciucu@gmail.com>
2020-01-29 16:33:22 +0000

    extract balance var and operations to History class

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -5,20 +5,7 @@ class Transaction
     def initialize
         @date = date
         @amount = amount
-        @balance = BigDecimal('0.00')
     end
-
-    def add(amount)
-        is_valid(amount)
-        @balance += amount
-    end
-
-    def withdraw(amount)
-        fail "Amount higher than current balance." unless (@balance - amount) >= 0
-        is_valid(amount)
-        @balance -= amount
-    end
-
 
     private
 
@@ -30,12 +17,5 @@ class Transaction
     def format_amount(amount)
         amount = BigDecimal('0.00')
     end
-
-    def is_valid(amount)
-        return unless amount.to_f.negative? 
-        # || amount.count("a-zA-Z") > 0
-        
-        raise StandardError.new "Invalid input."
-    end
-    
+      
 end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -17,47 +17,5 @@ describe Transaction do
     end
   end
 
-  describe "#deposit" do
-    it "should allow adding £5" do
-      subject.add(5.06)
-      expect(subject.balance).to eq 5.06
-    end
-
-    it "should allow adding multiple deposits" do
-      subject.add(5.06)
-      subject.add(5.00)
-      expect(subject.balance).to eq 10.06
-    end
-
-    it "should not allow adding a negative amount" do
-      expect { subject.add(-5.06) }.to raise_error "Invalid input."
-    end
-
-    xit "should not allow inputting letters" do
-      expect { subject.add("7Bc.00") }.to raise_error "Invalid input."
-    end
-
-  end
-
-  describe "#withdrawal" do
-    it "should allow withdrawing £5" do
-      subject.add(5.06)
-      subject.withdraw(5.00)
-      expect(subject.balance).to eq 0.06
-    end
-
-    it "should allow multiple withdrawals" do
-      subject.add(10.06)
-      subject.withdraw(5.00)
-      subject.withdraw(2.03)
-      expect(subject.balance).to eq 3.03
-    end
-
-    it "should raise an error if the withdrawn amount is higher than the balance" do
-      subject.add(10.06)
-      expect { subject.withdraw(10.20) }.to raise_error "Amount higher than current balance."
-    end
-  end
-
-
+  
 end
