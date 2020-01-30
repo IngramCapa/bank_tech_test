bd836a1cddce3c11c3a84c3d63a604bae6ad2870
Raluca Ciucu <raluca.ciucu@gmail.com>
2020-01-30 13:35:35 +0000

    test: failing atm, transactions stores more than one transaction

--- spec/transaction_history_spec.rb
+++ spec/transaction_history_spec.rb
@@ -13,7 +13,26 @@ describe TransactionHistory do
     describe "#add" do
         it "adds one transaction to the history" do
             expect(subject.add_transaction(800.00, "debit", 1000.00, transaction_class)).to eq transaction_1
-          
+        end
+
+        # it "adds more than one transaction to the history" do
+        #     expect(subject.add_transaction(1000.00, "credit", 1000.00, transaction_class)).to eq transaction_2
+
+        # end
+    end
+
+    describe "#transactions" do
+
+        before(:each) do
+            allow(transaction_1).to receive(:date) {"10/01/2012"}
+            allow(transaction_2).to receive(:date) {"13/01/2012"}
+            allow(transaction_class).to receive(:new).and_return([transaction_1, transaction_2])
+            subject.add_transaction(800.00, "debit", 1000.00, transaction_class)
+            subject.add_transaction(1000.00, "credit", 1000.00, transaction_class)
+        end
+
+        it "returns more than one stored transaction" do
+            expect(subject.transactions).to eq [transaction_2, transaction_1]
         end
     end
   
