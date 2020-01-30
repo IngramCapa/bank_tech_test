4874c4607167d1a9234564875dbe79a2acc8e747
Raluca Ciucu <raluca.ciucu@gmail.com>
2020-01-30 17:57:48 +0000

    test: print table header

---
+++ spec/print_statement_spec.rb
@@ -0,0 +1,15 @@
+require 'print_statement'
+
+describe PrintStatement do
+    let(:transaction_history) {double :transaction_history}
+    let(:transaction_1) {double :transaction, date: "10/01/2012", amount: 1000.00, type: "credit", balance: 1000.00}
+    let(:transaction_2) {double :transaction, date: "13/01/2012", amount: 800.00, type: "debit", balance: 200.00}
+    let(:subject) { PrintStatement.new(transaction_history)}
+
+    describe "#print_header" do
+        it "should print the table header" do
+            expect(subject.print_header).to output("date || credit || debit || balance\n").to_stdout
+        end
+    end
+
+end
